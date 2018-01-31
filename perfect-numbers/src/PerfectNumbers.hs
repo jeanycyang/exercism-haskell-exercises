30e474a79f18d85fe21c90dc1975294a4597ad24
Jean Yang <bdlas321@gmail.com>
2018-01-31 09:09:33 +0800

    PerfectNumbers: try using quotient but compiler returns an error
    "No instance for (Fractional Int) arising from a use of `/'"

--- perfect-numbers/src/PerfectNumbers.hs
+++ perfect-numbers/src/PerfectNumbers.hs
@@ -9,10 +9,19 @@ classify n
   | aliquotsSum > n = Just Abundant
   | aliquotsSum == n = Just Perfect
   | aliquotsSum < n = Just Deficient
-  where aliquotsSum = sum $ aliquots n
+  where aliquotsSum = (sum $ aliquots n) - n
 
 aliquots :: Int -> [Int]
 aliquots n
   | n <= 1 = []
-  | otherwise = [1,n]
+  | otherwise = findAliquots n 2 [n,1]
 
+findAliquots :: Int -> Int -> [Int] -> [Int]
+findAliquots n dividend output
+  | dividend == n = output
+  | (output !! 0 - output !! 1) <= 1 = output
+  | isInt quotient = findAliquots n (dividend+1) ([quotient,dividend]++output)
+  | otherwise = findAliquots n (dividend+1) output
+  where quotient = n / dividend
+
+isInt x = x == fromInteger (round x)
